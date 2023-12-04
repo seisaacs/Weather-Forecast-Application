79e956f8fb9bf40040c0346a69143042df813d22
seisaacs <151029994+seisaacs@users.noreply.github.com>
2023-12-04 08:57:46 -0500

    Added api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+function updateWeather(response) {}
+function searchCity(city) {
+  let apiKey = "439oc97e6c30db144fct5c81a42d9f2d";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function updateCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", updateCity);
