4ca74199721fd7a72f162fefa600542cf7d8a043
seisaacs <151029994+seisaacs@users.noreply.github.com>
2023-12-05 20:24:38 -0500

    Updated search function with API

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-function updateWeather(response) {}
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
 function searchCity(city) {
   let apiKey = "439oc97e6c30db144fct5c81a42d9f2d";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
@@ -8,9 +14,10 @@ function searchCity(city) {
 function updateCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", updateCity);
+
+searchCity("Miami");
