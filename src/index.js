c74c3345e6a1173b97a5847f08e4923d87661930
seisaacs <151029994+seisaacs@users.noreply.github.com>
2023-12-07 08:43:51 -0500

    Added js for icon

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ function updateWeather(response) {
   let windElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
   timeElement.innerHTML = formatDate(date);
   windElement.innerHTML = `${response.data.wind.speed}mph`;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
